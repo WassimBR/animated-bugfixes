65ce0c81db5c0cd9b32bf09588ac148fba9134db
wassim <wassim@wassim-VAIO>
2014-02-20 12:17:53 +0100

    Signed-off-by: wassim <wassim@wassim-VAIO>

--- tn.edu.esprit.gl8.portfolioAppClient/src/tn/edu/esprit/gl8/attarManClient/bestPractice/buisnesDelegate/CustomerDelegate.java
+++ tn.edu.esprit.gl8.portfolioAppClient/src/tn/edu/esprit/gl8/attarManClient/bestPractice/buisnesDelegate/CustomerDelegate.java
@@ -1,5 +1,21 @@
 package tn.edu.esprit.gl8.attarManClient.bestPractice.buisnesDelegate;
 
+import tn.edu.esprit.gl8.attarMan.domain.Customer;
+import tn.edu.esprit.gl8.attarMan.services.interfaces.CustomerManagmentRemote;
+import tn.edu.esprit.gl8.attarManClient.bestPractice.serviceLocater.ServiceLocater;
+
 public class CustomerDelegate {
+	private static final String jndiName = "ejb:/tn.edu.esprit.gl8.attarMan/CustomerManagement!"
+			+ CustomerManagmentRemote.class.getCanonicalName();
+
+	private static CustomerManagmentRemote getProxy() {
+		return (CustomerManagmentRemote) ServiceLocater.getInstance().getProxy(
+				jndiName);
+
+	}
+
+	public static boolean doAddCustomer(Customer customer) {
+		return getProxy().addCustomer(customer);
+	}
 
 }
